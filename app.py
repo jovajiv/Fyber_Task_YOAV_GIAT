185a8d5fc3e5f43384ea04268f29fa757cb9da28
Yoav Giat <jovajiv@gmail.com>
2020-10-08 13:11:14 +0300

    Delete excess code (2)

--- app.py
+++ app.py
@@ -11,11 +11,8 @@ VALUE=1
 
 @app.route('/')
 def hello_world():
-    response = requests.get("https://disease.sh/v3/covid-19/all")
-    print(type(response.json()))
-    return response.json()
-    #print (type(response.status_code))
-    #return 'Hello World!'
+    return json.dumps({})
+
 
 
 @app.errorhandler(404)
@@ -38,31 +35,6 @@ def status():
     except:
         return json.dumps({"status": "failure"})
 
-    #print (type(response.status_code))
-    #return 'Hello World!'
-
-
-
-
-
-#
-# @app.route('/newCasesPeakcountrys=<country>')
-# def newCasesPeak(country):
-#     try:
-#         response = requests.get("https://disease.sh/v3/covid-19/historical/"+country+"?lastdays=30")
-#         response.raise_for_status()
-#         if response.ok:
-#             ordered_json_response=response.json(object_pairs_hook=OrderedDict)['timeline']['cases']  #pairs are now orderd tuples.   OrderedDict([('9/7/20', 133975), ('9/8/20', 137565),....}
-#             list_of_pairs = list(ordered_json_response.items())              ## [('9/7/20', 133975), ('9/8/20', 137565), ('9/9/20', 141097),...] list format
-#             print(list_of_pairs)
-#             print(type(list_of_pairs))
-#             pair=max_pair_by_val(list_of_pairs)     # pair=(date_of_max_diff, max_diff)
-#             ordered_json_reply=OrderedDict([("country",country),("method","newCasesPeak"),("date",pair[DATE]), ("value",pair[VALUE])])
-#             return json.dumps(ordered_json_reply)
-#         else:
-#             return "API failed to respond"
-#     except requests.exceptions.HTTPError as e:
-#         return (e).__str__()
 
 
 #full url request :
@@ -108,24 +80,6 @@ def queryType(type):
 
 
 
-# @app.route('/deathsPeakcountrys=<country>')
-# def recoveredPeak(country):
-#     try:
-#         response = requests.get("https://disease.sh/v3/covid-19/historical/"+country+"?lastdays=30")
-#         response.raise_for_status()
-#         if response.ok:
-#             ordered_json_response=response.json(object_pairs_hook=OrderedDict)['timeline']['deaths']  #pairs are now orderd tuples.   OrderedDict([('9/7/20', 133975), ('9/8/20', 137565),....}
-#             list_of_pairs = list(ordered_json_response.items())              ## [('9/7/20', 133975), ('9/8/20', 137565), ('9/9/20', 141097),...] list format
-#             print(list_of_pairs)
-#             print(type(list_of_pairs))
-#             pair=max_pair_by_val(list_of_pairs)     # pair=(date_of_max_diff, max_diff)
-#             ordered_json_reply=OrderedDict([("country",country),("method","newCasesPeak"),("date",pair[DATE]), ("value",pair[VALUE])])
-#             return json.dumps(ordered_json_reply)
-#         else:
-#             return "API failed to respond"
-#     except requests.exceptions.HTTPError as e:
-#         return (e).__str__()
-
 
 
 #this function receives a list of tuple pairs. [(key1,val1) , (key2,val2) , (key3,val3)...]
@@ -141,30 +95,7 @@ def max_pair_by_val(list):
 
     return (list[max_index][DATE],max_diff)
 
-#def using_reduce(list):
-#    max_diff=0;
-#    print (functools.reduce(lambda pair1, pair2: (pair2[DATE],max_diff) if pair1[VALUE]-pair2[VALUE] > max_diff  , list))
-
-
-
-@app.route('/code')
-def hello_world2():
-    temp = request.args.get('qqq',None)
-    print(temp)
-    response = requests.get("https://disease.sh/v3/covid-19/all")
-    print(type(response.json()))
-    return jsonify(response.status_code)
-    #print (type(response.status_code))
-    #return 'Hello World!'
-
-
-def pop_first_item(dict):
-    return dict.popitem(last=False);
-
-
 
 if __name__ == '__main__':
     app.run(port=8080)
 
-#TypeError: The view function did not return a valid response. The return type must be a string, dict, tuple, Response instance, or WSGI callable, but it was a int.
-#127.0.0.1 - - [07/Oct/2020 09:45:46] "GET / HTTP/1.1" 500 -
