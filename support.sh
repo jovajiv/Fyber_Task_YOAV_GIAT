a15f19010f151d791881cc56d76a2018eca8778e
Yoav Giat <jovajiv@gmail.com>
2020-10-09 01:09:44 +0300

    temporary change code to test

--- support.sh
+++ support.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-netstat -tulpn | grep :80
 echo 'running web server in background ...'
 python app.py &
 
-netstat -tulpn | grep :80
+
 echo 'running curl'
-curl localhost:8080/newCasesPeak?country=israel
+curl localhost:1000/newCasesPeak?country=israel
